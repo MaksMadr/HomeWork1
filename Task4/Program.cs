93c8fe5c44f204a07a4b6a8c2344af21f092c908
MaksMadr <mvpanarin82@gmail.com>
2023-05-27 16:31:14 +0800

    task4

---
+++ Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.Write("Введите 1-е число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-е число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 3-е число: ");
+int c = int.Parse(Console.ReadLine()!);
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.WriteLine("Максимальное число: " + a);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число: " + c);
+    }
+}
+else if (b > c)
+{
+    Console.WriteLine("Максимальное число: " + b);
+}
+else
+{
+    Console.WriteLine("Максимальное число: " + c);
+}
