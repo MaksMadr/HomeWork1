53ade91f1e19ce99086a23b3d1392c666921cec7
MaksMadr <mvpanarin82@gmail.com>
2023-05-27 16:15:11 +0800

    task2

---
+++ Task2/Program2.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите 1-е число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-е число: ");
+int m = int.Parse(Console.ReadLine()!);
+if (n > m)
+    Console.WriteLine($"{n} больше {m}");
+else if (m > n)
+    Console.WriteLine($"{m} больше {n}");
+else
+    Console.WriteLine($" 1-ое число {m} равно 2-ому числу {n}");
